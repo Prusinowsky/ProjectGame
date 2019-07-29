1efc502a02f75665ad173ff5d9da17fa6d16a2da
Sienq <Hubert.Sienkiewicz99@gmail.com>
2019-07-29 15:12:32 +0200

    Added drawSquare method

--- Drawer.cpp
+++ Drawer.cpp
@@ -1,4 +1,5 @@
 #include "Drawer.h"
+#include "Game.h"
 
 Drawer* Drawer::instance = nullptr;
 
@@ -13,6 +14,21 @@ Drawer * Drawer::getInstance()
 		instance = new Drawer;
 	return instance;
 }
+
+
+void Drawer::drawSquare(sf::Vector2i position, sf::Color color)
+{
+	sf::RectangleShape square;
+
+	square.setPosition(static_cast<sf::Vector2f>(position));
+	square.setFillColor(color);
+
+	auto Game = Game::getInstance();
+	auto window = Game->getWindow();
+
+	window->draw(square);
+}
+
 
 
 Drawer::~Drawer()
