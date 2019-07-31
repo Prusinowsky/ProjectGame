12587279b27e702ae0582741ceaa60f431eae4ba
Sienq <Hubert.Sienkiewicz99@gmail.com>
2019-07-31 13:29:07 +0200

    Cosmetical changes

--- ResourcesManager.cpp
+++ ResourcesManager.cpp
@@ -1,46 +1,46 @@
-#include "ResourcesMenager.h"
+#include "ResourcesManager.h"
 
 
 
-ResourcesMenager::ResourcesMenager()
+ResourcesManager::ResourcesManager()
 {
 }
 
-void ResourcesMenager::loadFont(std::string fontName, std::string fileName)
+void ResourcesManager::loadFont(std::string fontName, std::string fileName)
 {
 	sf::Font fontToLoad;
 	if (fontToLoad.loadFromFile(fileName)) this->fonts[fontName] = fontToLoad;  ///Loading font from file and adding it to map
 	else std::cout << "Loading error, font: " << fileName << std::endl; ///Error message
 }
 
-void ResourcesMenager::deleteFont(std::string fontName)
+void ResourcesManager::deleteFont(std::string fontName)
 {
-	this->fonts.erase(fontName);
+	this->fonts.erase(fontName); ///Deleting font from map
 }
 
-sf::Font & ResourcesMenager::getFont(std::string fontName)
+const sf::Font ResourcesManager::getFont(std::string fontName)
 {
 	return this->fonts.at(fontName); ///Returning font that we want
 }
 
-void ResourcesMenager::loadTexture(std::string textureName, std::string filename)
+void ResourcesManager::loadTexture(std::string textureName, std::string filename)
 {
 	sf::Texture textureToLoad;
 	if (textureToLoad.loadFromFile(filename)) this->textures[textureName] = textureToLoad;  ///Loading font from file and adding it to map
 	else std::cout << "Loading error, texture: " << filename << std::endl; ///Error message
 }
 
-void ResourcesMenager::deleteTexture(std::string textureName)
+void ResourcesManager::deleteTexture(std::string textureName)
 {
-	this->textures.erase(textureName);
+	this->textures.erase(textureName); ///Deleting texture from map
 }
 
-sf::Texture & ResourcesMenager::getTexture(std::string textureName)
+const sf::Texture ResourcesManager::getTexture(std::string textureName)
 {
 	return this->textures.at(textureName); ///Returning texture that we want
 }
 
 
-ResourcesMenager::~ResourcesMenager()
+ResourcesManager::~ResourcesManager()
 {
 }
--- ResourcesManager.h
+++ ResourcesManager.h
@@ -2,19 +2,19 @@
 #include <SFML/Graphics.hpp>
 #include <map>
 #include <iostream>
-class ResourcesMenager
+class ResourcesManager
 {
 public:
-	ResourcesMenager();
+	ResourcesManager();
 	void loadFont(std::string fontName,std::string fileName);
 	void deleteFont(std::string fontName);
-	sf::Font &getFont(std::string fontName);
+	const sf::Font getFont(std::string fontName);
 
 	void loadTexture(std::string textureName, std::string filename);
 	void deleteTexture(std::string textureName);
-	sf::Texture &getTexture(std::string textureName);
+	const sf::Texture getTexture(std::string textureName);
 
-	~ResourcesMenager();
+	~ResourcesManager();
 private:
 	std::map <std::string,sf::Font> fonts;
 	std::map <std::string,sf::Texture> textures;
